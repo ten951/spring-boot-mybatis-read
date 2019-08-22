65b9cfdb25f446e927c654d106bf3a420859484a
wangyongtian <ten951@qq.com>
2019-08-22 17:53:01 +0800

    project initiate

---
+++ src/main/java/com/ten951/boot/mybatis/read/SpringBootMybatisReadApplication.java
@@ -0,0 +1,13 @@
+package com.ten951.boot.mybatis.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ten951/boot/mybatis/read/SpringBootMybatisReadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ten951.boot.mybatis.read;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMybatisReadApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
